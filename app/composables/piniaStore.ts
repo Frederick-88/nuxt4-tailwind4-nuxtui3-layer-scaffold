7a437a22c64e09e0df19ab197e88249926148f44
frederick-88 <frederickfd88@gmail.com>
2025-11-07 18:20:29 +0700

    chore: minor

--- app/composables/piniaStore.ts
+++ app/composables/piniaStore.ts
@@ -1,3 +1,5 @@
+// todo fred note: wait for reply/confirmation from pinia team on https://github.com/vuejs/pinia/pull/3035#issuecomment-3493468703
+
 // import { useSharedModalStore } from "@/store/modal";
 // import { useSharedWalletStore } from "@/store/wallet";
 
